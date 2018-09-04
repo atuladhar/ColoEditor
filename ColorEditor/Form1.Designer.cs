a7874346f3d03e3bdd72732b0e3b3bc878000b67
Archana <tuladhar.archana@gmail.com>
2018-09-04 17:13:10 +0545

    First Commit

---
+++ ColorEditor/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ColorEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.fluentTheme1 = new Telerik.WinControls.Themes.FluentTheme();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.elementHost1.Location = new System.Drawing.Point(0, 0);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(1190, 710);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1190, 710);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Form1";
+            this.ThemeName = "Fluent";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private Telerik.WinControls.Themes.FluentTheme fluentTheme1;
+    }
+}
+
---
+++ ColorEditor/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+using Telerik.WinControls.UI;
+using WpfApp2;
+
+namespace ColorEditor
+{
+    public partial class Form1 : RadForm
+    {
+        CustomControl control=new CustomControl();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //elementHost1.Child = control;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ElementHost host = new ElementHost();
+            host.Child = control;
+            host.Dock = DockStyle.Fill;
+            this.Controls.Add(host);
+
+           //elementHost1.Child = control;
+           // var g = elementHost1.CreateGraphics();
+           // g.Dispose();
+        }
+    }
+}
